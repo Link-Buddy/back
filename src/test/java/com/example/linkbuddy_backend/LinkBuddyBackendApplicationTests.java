25b1ed90ed18d55b724a0b7d9020a05e44a62333
summer <80736033+yl9517@users.noreply.github.com>
2024-01-20 19:06:38 +0900

    spring boot project 생성

---
+++ src/test/java/com/example/linkbuddy_backend/LinkBuddyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linkbuddy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkBuddyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
